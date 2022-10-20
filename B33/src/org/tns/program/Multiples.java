b51335a5d6bbbcaf66375096d50fc9a7852c4c0c
Sakthi <Sakthi@DESKTOP-VQ7I5LN>
2022-10-20 11:40:28 +0530

    first commit

---
+++ B33/src/org/tns/program/Multiples.java
@@ -0,0 +1,13 @@
+package org.tns.program;
+
+public class Multiples {
+	   public static void main(String[] args) {
+	       int a;
+	       int b;
+	       int c;
+	       a = 5;
+	       b = 58;
+	       c = a*b; //integer number to keep the result of multiplication
+	       System.out.println("5*58 = " + c);
+	   }
+	}
