a66529040f906162aa15f2bb63e46b5e38303d8b
Sakthi <Sakthi@DESKTOP-VQ7I5LN>
2022-10-20 12:05:06 +0530

    first commit

---
+++ B33/src/org/tns/program/Typecasting.java
@@ -0,0 +1,16 @@
+package org.tns.program;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		float f=10.532f;
+		long l=(long)f;
+		double d=f;
+		byte b=12;
+		short s=b;
+		int a=1;
+		System.out.println(l);
+
+	}
+
+}
