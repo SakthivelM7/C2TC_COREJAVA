9f885c24d2c64d050b043cec92b47c68c617f2c2
Sakthi <Sakthi@DESKTOP-VQ7I5LN>
2022-10-20 15:16:11 +0530

    first commit

---
+++ B33/src/org/tns/program/Decisionoperators2.java
@@ -0,0 +1,20 @@
+package org.tns.program;
+
+public class Decisionoperators2 {
+
+	public static void main(String[] args) {
+		int x=8,y=7;
+		if(x>=y)
+		{
+			System.out.println("true");
+			
+		}
+		else
+		{
+			System.out.println("false");
+		}
+		 	
+
+	}
+
+}
