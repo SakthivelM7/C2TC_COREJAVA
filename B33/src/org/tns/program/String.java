ab08b5c6091075635012e1e78193fc1f0501d659
Sakthi <Sakthi@DESKTOP-VQ7I5LN>
2022-10-20 15:37:00 +0530

    First commit

---
+++ B33/src/org/tns/program/String.java
@@ -0,0 +1,13 @@
+package org.tns.program;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		String var=new String();
+		System.out.println("Hello World");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
