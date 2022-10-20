eea4a9e0b49c4cf59fcfc3ebb58728e52332d884
Sakthi <Sakthi@DESKTOP-VQ7I5LN>
2022-10-20 11:26:21 +0530

    First commit

---
+++ B33/src/org/tns/program/Arithmeticoperators.java
@@ -0,0 +1,14 @@
+package org.tns.program;
+
+public class Arithmeticoperators {
+
+	public static void main(String[] args) {
+		int x = 15, y = 20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- B33/src/org/tns/program/Firstprogram.java
+++ B33/src/org/tns/program/Firstprogram.java
@@ -3,7 +3,7 @@ package org.tns.program;
 public class Firstprogram {
 
 	public static void main(String[] args) {
-		System.out.println("Sakthivel M");
+		 
 		// TODO Auto-generated method stub
 
 	}
